dc275b92f7158b6c501b93338021894ba5b68a06
Armando Santos <armandoifsantos@gmail.com>
2019-04-14 16:51:21 +0100

    Implemented the discovery protocol.
    Now if the light bulb for some reason changes its IP address
    the program is going to scan the network using SSDP and try to find
    a bulb.

--- src/Main.hs
+++ src/Main.hs
@@ -4,8 +4,10 @@ module Main (main) where
 
 import qualified Control.Exception as E
 import qualified Data.ByteString.Char8 as C
+import qualified Data.ByteString as B
+import Control.Monad
 import Network.Socket hiding (recv, sendAll)
-import Network.Socket.ByteString (recv, sendAll)
+import Network.Socket.ByteString (recv, sendAll, sendTo, recvFrom)
 import Network.Multicast
 import System.Directory
 import System.Environment
@@ -23,6 +25,13 @@ getAddress = do
              else do
              writeFile fileP "127.0.0.1\n55443"
              return ("127.0.0.1", "55443")
+
+updateAddress :: HostName -> IO ()
+updateAddress ip = do
+        homeDir <- getHomeDirectory
+        let fileP = homeDir ++ "/.yeelight_wrapper.config"
+        writeFile fileP $ ip ++ "\n55443"
+
 
 resolve :: HostName -> ServiceName -> IO AddrInfo
 resolve host port = do
@@ -43,20 +52,28 @@ talk sock cmd = do
         putStr "Received: "
         C.putStrLn msg
 
-discoveryProtocol :: String
-discoveryProtocol = "M-SEARCH * HTTP/1.1\r\n" ++
+discoveryProtocol :: C.ByteString
+discoveryProtocol = C.pack ("M-SEARCH * HTTP/1.1\r\n" ++
     "HOST: 239.255.255.250:1982\r\n" ++
     "MAN: \"ssdp:discover\"\r\n" ++
-    "ST: wifi_bulb"
+    "ST: wifi_bulb")
 
-{-
-discover :: IO HostName
-discover = withSocketsDo $ do
-        addr <- resolve "239.255.255.250" 1982
-        connect sock $ "239.255.255.250"
-    where
-        multicastSocket = socket AF_INET Datagram 1982
-        -}
+-- (2) Discovery functions -----------
+
+discover :: AddrInfo -> IO Socket
+discover _ = withSocketsDo $ do
+        (sSock, sAddr) <- multicastSender "239.255.255.250" 1982
+        sendTo sSock discoveryProtocol sAddr
+        (msg, rAddr)              <- recvFrom sSock 1024
+        let (SockAddrInet _ host) = rAddr
+            (w1, w2, w3, w4)      = hostAddressToTuple host
+            (i1, i2, i3, i4)      = (fromIntegral w1, fromIntegral w2, fromIntegral w3,
+                                     fromIntegral w4)
+            ip                    = show i1 ++ "." ++ show i2 ++ "." ++ show i3 ++ "." ++ show i4
+        addr                      <- resolve ip "55443"
+        updateAddress ip
+        open addr
+
 
 -- (2) Main program -----------
 
@@ -64,10 +81,11 @@ main :: IO ()
 main = withSocketsDo $ do
     cmd <- getArgs
     (host, port) <- getAddress
-    addr <- resolve host port
-    sock <- open addr 
-    case cmd of
-        ["off"] -> talk sock "{\"id\": 1, \"method\":\"set_power\", \"params\":[\"off\"]}\r\n"
-        ["on"]  -> talk sock "{\"id\": 1, \"method\":\"set_power\", \"params\":[\"on\"]}\r\n"
-        _       -> talk sock "{\"id\": 1, \"method\":\"toggle\", \"params\":[]}\r\n"
-    close sock
+    E.bracketOnError (resolve host port) (loop cmd <=< discover) (loop cmd <=< open)
+ where
+     loop cmd sock = do
+         case cmd of
+             ["off"] -> talk sock "{\"id\": 1, \"method\":\"set_power\", \"params\":[\"off\"]}\r\n"
+             ["on"]  -> talk sock "{\"id\": 1, \"method\":\"set_power\", \"params\":[\"on\"]}\r\n"
+             _       -> talk sock "{\"id\": 1, \"method\":\"toggle\", \"params\":[]}\r\n"
+         close sock
